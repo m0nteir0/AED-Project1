8a65ac26a804e663bb559b1484b09a697445d85f
carol viana <carolinacoutoviana@gmail.com>
2022-10-11 14:06:32 +0100

    Só tem duas classes inuteis

---
+++ Class.cpp
@@ -0,0 +1,3 @@
+//
+// Created by carol on 11/10/2022.
+//
---
+++ Class.h
@@ -0,0 +1,8 @@
+t p//
+// Created by carol on 11/10/2022.
+//
+
+#ifndef AED_PROJECT1_CLASS_H
+#define AED_PROJECT1_CLASS_H
+
+#endif //AED_PROJECT1_CLASS_H
